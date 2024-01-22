20729f6a2a128970a58941688b3159b3179781f6
dohyun <thtree523@gmail.com>
2024-01-22 23:52:55 +0900

    [FEAT] 카카오 로그인, 구글 로그인 기능 추가

--- AuthBoilerplate/AppDelegate.swift
+++ AuthBoilerplate/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import KakaoSDKCommon
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        KakaoSDK.initSDK(appKey: NATIVE_APIKEY)
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+              // Show the app's signed-out state.
+            } else {
+              // Show the app's signed-in state.
+            }
+        }
         return true
     }
 
@@ -29,6 +38,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
+
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        return false
     }
 
 
--- AuthBoilerplate/ViewController.swift
+++ AuthBoilerplate/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import KakaoSDKUser
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var kakaoLoginButton: UIButton!
+    @IBOutlet weak var GoogleLoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func kakaoLoginClick(_ sender: Any) {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
 
+                    //do something
+                    _ = oauthToken
+                }
+            }
+        } else {
+            var scopes: [String] = ["profile_nickname", "account_email", "name", "openid"]
+            UserApi.shared.loginWithKakaoAccount(scopes: scopes) { (_, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("test")
+                    UserApi.shared.me() { (user, error) in
+                        if let error = error {
+                            print(error)
+                        }
+                        else {
+                            print("me() success.")
+                            
+                            //do something
+                            _ = user
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @IBAction func googleLoginClick(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+            let profile = signInResult?.user.profile
+            print(profile?.name)
+            print(profile?.email)
+        }
+    }
+    
 
 }
 
