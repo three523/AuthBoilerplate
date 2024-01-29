da533e50dabe39aeb9051140680213e95ec497c1
dohyun <thtree523@gmail.com>
2024-01-29 22:45:47 +0900

    [FEAT] info.plist 구글, 카카오 기본 셋팅

--- Sources/GoogleAuthInfo.swift
+++ Sources/GoogleAuthInfo.swift
@@ -10,22 +10,21 @@ import Foundation
 struct GoogleAuthInfo: AuthInfo {
     
     func setup(plistDict: NSMutableDictionary) -> NSMutableDictionary {
-//        while true {
-//            print("iOS Client ID를 입력해주세요")
-//            guard let clientID = readLine() else { continue }
-//            var newPlistDict = addClientID(plistDict: plistDict, iosClientID: clientID)
-//            print("iOS URL 스키마를 입력해주세요")
-//            guard let urlScheme = readLine() else { continue }
-//            newPlistDict = addURLTypes(plistDict: newPlistDict, reversedIosClientID: urlScheme)
-//            print("서버 클라이언트 아이디가 있는 경우 입력해주세여(없을 경우 엔터)")
-//            guard let serverClientID = readLine() else { continue }
-//            guard serverClientID == "" else {
-//                newPlistDict = addServerClientID(plistDict: newPlistDict, serverClientID: serverClientID)
-//                return newPlistDict
-//            }
-//            break
-//        }
-        insertGoogleInitCode()
+        while true {
+            print("[구글] iOS Client ID를 입력해주세요")
+            guard let clientID = readLine() else { continue }
+            var newPlistDict = addClientID(plistDict: plistDict, iosClientID: clientID)
+            print("[구글] iOS URL 스키마를 입력해주세요")
+            guard let urlScheme = readLine() else { continue }
+            newPlistDict = addURLTypes(plistDict: newPlistDict, reversedIosClientID: urlScheme)
+            print("[구글] 서버 클라이언트 아이디가 있는 경우 입력해주세여(없을 경우 엔터)")
+            guard let serverClientID = readLine() else { continue }
+            guard serverClientID == "" else {
+                newPlistDict = addServerClientID(plistDict: newPlistDict, serverClientID: serverClientID)
+                return newPlistDict
+            }
+            break
+        }
         return plistDict
     }
     
@@ -54,37 +53,5 @@ struct GoogleAuthInfo: AuthInfo {
         let newPlistDict = plistDict
         newPlistDict["GIDServerClientID"] = serverClientID
         return newPlistDict
-    }
-    
-    private func insertGoogleInitCode() {
-        print("Google Login을 위한 Appdelegate 셋팅 중")
-        let code = """
-        \n\t\tGIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
-            \t\tif error != nil || user == nil {
-              \t\t\t// Show the app's signed-out state.
-            \t\t} else {
-              \t\t\t// Show the app's signed-in state.
-            \t\t}
-        \t\t}
-        """
-        let googleSignInHandleCode = """
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        \tvar handled: Bool
-
-        \thandled = GIDSignIn.sharedInstance.handle(url)
-        \tif handled {
-            \treturn true
-        \t}
-        \treturn false
-    \t}\n\n\t
-    """
-        
-        let defaultPath = FileManager.default.currentDirectoryPath
-        let projectName = URL(fileURLWithPath: defaultPath).lastPathComponent
-        let appdelegateFilePath = defaultPath + "/\(projectName)/Appdelegate.swift"
-        
-        insertCodeIntoFile(filePath: appdelegateFilePath, keyword: "UIKit", codeToInsert: "\nimport GoogleSignIn", insertPosition: .back)
-        insertCodeIntoFile(filePath: appdelegateFilePath, keyword: "LaunchOptionsKey: Any]?) -> Bool {", codeToInsert: code, insertPosition: .back)
-        insertCodeIntoFile(filePath: appdelegateFilePath, keyword: "func", codeToInsert: googleSignInHandleCode, insertPosition: .front)
     }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,12 @@
+//
+//  main.swift
+//  Setup
+//
+//  Created by 김도현 on 2024/01/29.
+//
+
+import Foundation
+
 import Foundation
 
 enum ReadLineType: Int {
@@ -8,103 +17,18 @@ enum ReadLineType: Int {
 
 let defaultPath = FileManager.default.currentDirectoryPath
 let projectName = URL(fileURLWithPath: defaultPath).lastPathComponent
-let packagePath = defaultPath + "/Package.swift"
 let plistPath = defaultPath + "/\(projectName)/info.plist"
-var dependencies = [String]()
-
-func insertCodeIntoFile(filePath: String, keyword: String, codeToInsert: String, insertPosition: InsertPosition) {
-    do {
-        
-        if FileManager.default.fileExists(atPath: filePath) {
-            var fileContent = try String(contentsOfFile: filePath)
-            
-            if let range = fileContent.range(of: keyword) {
-                fileContent.insert(contentsOf: codeToInsert, at: insertPosition == .front ? range.lowerBound : range.upperBound)
-                
-                try fileContent.write(toFile: filePath, atomically: true, encoding: .utf8)
-                
-                print("코드 삽입 성공.")
-            } else {
-                print(keyword)
-                print("키워드를 파일에서 찾을수 없습니다.")
-            }
-        } else {
-            try codeToInsert.write(toFile: filePath, atomically: true, encoding: .utf8)
-            print("APIKEY 생성 완료")
-        }
-    } catch {
-        print("Error: \(error)")
-    }
-        
-}
-
-func shell(_ args: String...) -> (output: String, exitCode: Int32) {
-    let task = Process()
-    task.launchPath = "/usr/bin/env"
-    task.arguments = args
-    task.currentDirectoryPath = defaultPath
-    let pipe = Pipe()
-    task.standardOutput = pipe
-    task.launch()
-    task.waitUntilExit()
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8) ?? ""
-    return (output, task.terminationStatus)
-}
-
-func addPackage(packages: [String], librayNames:[String]) {
-    var dependenciesPackageCode = ""
-    var dependenciesTargetCode = ""
-    for package in packages {
-        dependenciesPackageCode += "\n\t\t\(package)"
-    }
-    for librayName in librayNames {
-        dependenciesTargetCode += "\"\(librayName)\","
-    }
-    var dependenciesCode = """
-    \n\tdependencies: [\(dependenciesPackageCode)
-    ],
-    """
-    var targetCode = """
-    .target(
-    \t\t\tname: \"\(projectName)\",
-    \t\t\tdependencies: [\(dependenciesTargetCode)]),\n\t\t
-    """
-    insertCodeIntoFile(filePath: packagePath, keyword: "\(projectName)\",", codeToInsert: dependenciesCode, insertPosition: .back)
-    insertCodeIntoFile(filePath: packagePath, keyword: ".executa", codeToInsert: targetCode, insertPosition: .front)
-    
-    shell("swift", "package", "update")
-}
-
-let packages = [".package(url: \"https://github.com/Alamofire/Alamofire.git\", .branch(\"master\")),",
-".package(url: \"https://github.com/google/GoogleSignIn-iOS\", .upToNextMajor(from: \"7.0.0\"))"]
-
-let librayNames = ["KakaoSDK", "GoogleSignIn"]
-addPackage(packages: packages, librayNames: librayNames)
 
 // Info.plist 파일을 딕셔너리로 읽어오기
-//guard var plistDict = NSMutableDictionary(contentsOfFile: plistPath) else {
-//    fatalError("Error: Unable to read Info.plist.")
-//}
-//
-//var infoSetup: InfoSetup = InfoSetup(plistDict: plistDict)
-//
-//while true {
-//    print("소셜 로그인을 선택해주세요 (숫자를 입력해주세요)")
-//    print("1.kakao 2.google 9.리셋 0.종료")
-//    guard let type = Int(readLine() ?? ""),
-//          let readLienType = ReadLineType(rawValue: type) else { continue }
-//    if readLienType == .end { break }
-//    else if readLienType == .kakao {
-//        infoSetup.kakao()
-//        continue
-//    } else if readLienType == .google {
-//        infoSetup.google()
-//        continue
-//    }
-//    break
-//}
-//
-//let newPlistDict = infoSetup.create()
-//
-//newPlistDict.write(toFile: plistPath, atomically: true)
+guard var plistDict = NSMutableDictionary(contentsOfFile: plistPath) else {
+    fatalError("Error: Unable to read Info.plist.")
+}
+
+var infoSetup: InfoSetup = InfoSetup(plistDict: plistDict)
+
+infoSetup.kakao()
+infoSetup.google()
+
+let newPlistDict = infoSetup.create()
+
+newPlistDict.write(toFile: plistPath, atomically: true)
